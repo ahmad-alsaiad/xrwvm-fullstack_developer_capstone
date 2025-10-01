6a6b89b4a17d49db5a029776421fb466017d62fa
ahmad <ahmadalsaiad2007@gmail.com>
2025-10-01 08:10:26 -0400

    feat: Implemented CarMake and CarModel ORM objects and configured custom admin inlines

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,6 +6,7 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
     path(route='logout', view=views.logout_request, name='logout'),
     # path for registration
     path(route='register', view=views.registration, name='register'),
